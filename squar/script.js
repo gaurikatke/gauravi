d7ec75c31e82463bcf4d54f728e0ca52cc173ab8
gauri <gauravikatke@gmail.com>
2024-07-26 15:06:48 +0530

    project complete

--- squar/script.js
+++ squar/script.js
@@ -1,16 +1,23 @@
 const squareArray = document.getElementsByClassName ("square");
+
 const colors = ["#e74c3c","#8e44ad","#3498db","#e67e22","#2ecc71"];
-const defaultcolor = "rgba(255,255,255,0.1)"
+
+const defaultColor = "rgba(255,255,255,0.1)";
 for (const square of squareArray){
     square.addEventListener("mouseenter",()=>{
         const randomNum = parseInt(Math.random()*colors.length);
+
         square.style.background= colors [randomNum];
+
         square.style.transition = "background-color 0s ease";
+
         square.style.boxshadow = "0px 0px 10px"+ colors[randomNum];
     });
+
     square.addEventListener("mouseleave",()=>{
-        square.style.backgroundcolor = defaultcolor;
-        square.style.transition= "background-color 1s ease";
+        square.style.backgroundColor = defaultColor;
+
+        square.style.transition= "background-color 2s ease";
         square.style.boxshadow=""; 
     });
 }
